50fc3f3217536b2ca101e21b89839f9531c4d8d6
Harikrishnan <hknp796@gmail.com>
2022-06-01 20:28:51 +0530

    feat: home page added

---
+++ components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="d-flex align-center justify-space-around mt-13">
+      <h1>SOLO</h1>
+      <div class="text-field">
+        <v-text-field
+          placeholder="I'm Looking for"
+          solo
+          rounded
+          append-icon="mdi-magnify"
+          hide-details="true"
+        ></v-text-field>
+      </div>
+      <div class="d-flex">
+        <v-icon> mdi-phone </v-icon>
+        <v-icon class="ml-12"> mdi-account </v-icon>
+        <v-icon class="ml-12"> mdi-cart-outline </v-icon>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.text-field {
+  width: 400px;
+}
+</style>
---
+++ components/Products.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-btn @click="getData"> click </v-btn>
+  </div>
+</template>
+
+<script>
+import axios from '@nuxtjs/axios'
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    getData() {
+      const options = {
+        method: 'GET',
+        url: 'https://taobao-api.p.rapidapi.com/api',
+        params: { api: 'item_search', page_size: '40', q: 'shoes' },
+        headers: {
+          'X-RapidAPI-Host': 'taobao-api.p.rapidapi.com',
+          'X-RapidAPI-Key':
+            '361da33457msh321bb58d0e43461p1ecf11jsn542819c4b882',
+        },
+      }
+
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data)
+        })
+        .catch(function (error) {
+          console.error(error)
+        })
+    },
+  },
+}
+</script>
