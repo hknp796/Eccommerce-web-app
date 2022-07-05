253ec2ac2843445f17115bd1d146548b6eae60be
hknp796 <foslux@gamail.com>
2022-07-05 23:10:34 +0530

    feat: single product added

--- components/Products.vue
+++ components/Products.vue
@@ -5,33 +5,10 @@
 </template>
 
 <script>
-import axios from '@nuxtjs/axios'
 export default {
   data() {
     return {}
   },
-  methods: {
-    getData() {
-      const options = {
-        method: 'GET',
-        url: 'https://taobao-api.p.rapidapi.com/api',
-        params: { api: 'item_search', page_size: '40', q: 'shoes' },
-        headers: {
-          'X-RapidAPI-Host': 'taobao-api.p.rapidapi.com',
-          'X-RapidAPI-Key':
-            '361da33457msh321bb58d0e43461p1ecf11jsn542819c4b882',
-        },
-      }
-
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data)
-        })
-        .catch(function (error) {
-          console.error(error)
-        })
-    },
-  },
+  methods: {},
 }
 </script>
--- pages/ProductDetails/_id.vue
+++ pages/ProductDetails/_id.vue
@@ -1,14 +1,10 @@
 <template>
   <div>
-    <div v-if="getProductsData[0]" class="d-flex wrapper">
-      <v-img
-        :src="getProductsData[0].imageURL"
-        width="100"
-        height="400"
-      ></v-img>
+    <div v-if="getProductsData" class="d-flex wrapper">
+      <v-img :src="getProductsData.imageURL" width="100" height="400"></v-img>
       <div class="ml-10">
-        <h4>{{ getProductsData[0].name }}</h4>
-        <h5>$ {{ getProductsData[0].price }}</h5>
+        <h4>{{ getProductsData.name }}</h4>
+        <h5>$ {{ getProductsData.price }}</h5>
         <div class="d-flex align-center">
           <h4>Size</h4>
           <div class="size">6</div>
@@ -23,15 +19,17 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      getProductsData: [],
+    }
   },
 
-  computed: {
-    getProductsData() {
-      return this.$store.state.products.filter(
-        (item) => item.id === Number(this.$route.params.details)
-      )
-    },
+  async created() {
+    this.getProductsData = await this.$store.dispatch(
+      'fetchSingleproduct',
+      this.$route.params.id
+    )
+    console.log(this.$store.state.singleProduct)
   },
 }
 </script>
