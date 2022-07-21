2820315d0bf8790a82f6cd967b00038faeb1c71e
Alex <alex@localhost.localdomain>
2022-07-21 22:16:39 +0530

    feat: men and women products

---
+++ pages/menProducts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ men }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      men: '',
+    }
+  },
+  async mounted() {
+    this.men = await this.$store.dispatch('getMenProducts')
+  },
+}
+</script>
