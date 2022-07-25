9206fb4ca7ed1d583a3ae2d4d1e04f4c520da723
Harikrishnan <foslux@gmail.com>
2022-07-25 23:31:06 +0530

    feat: product list page

--- components/productList.vue
+++ components/productList.vue
@@ -1,35 +1,82 @@
 <template>
   <div>
-    {{ products }}
-    <div>
-      <v-row>
-        <v-col v-for="product in products" :key="product._id" cols="4">
-          <v-img
-            :src="product.img[0]"
-            width="300"
-            height="300"
-            class=""
-          ></v-img>
-          <h4>{{ product.title }}</h4>
-          <h4>{{ product.description }}</h4>
-          <h4>{{ product.price }}</h4>
-        </v-col>
-      </v-row>
-    </div>
+    <v-row>
+      <v-col cols="3">
+        <!-- {{ products }} -->
+
+        <v-list>
+          <v-list-group v-for="item in items" :key="item.title">
+            <template #activator>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item v-for="child in item.items" :key="child.title">
+              <v-checkbox
+                v-model="newone"
+                :label="child.title"
+                hide-details="true"
+                @change="check(child.title)"
+              ></v-checkbox>
+            </v-list-item>
+          </v-list-group>
+        </v-list>
+      </v-col>
+      <v-col cols="9">
+        <v-row>
+          <v-col v-for="product in products" :key="product._id" cols="4">
+            <v-img :src="product.img[0]" width="100%" height="auto"></v-img>
+            <h4>{{ product.title }}</h4>
+            <p>{{ product.description }}</p>
+            <h4>{{ product.price }}</h4>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </div>
 </template>
-
 <script>
 export default {
   props: {
     products: { type: Array },
   },
+
   data() {
-    return {}
+    return {
+      items: [
+        {
+          items: [{ title: 'Small' }, { title: 'Medium' }, { title: 'Large' }],
+          title: 'Size',
+        },
+
+        {
+          items: [
+            { title: 'White' },
+            { title: 'Black' },
+            { title: 'Red' },
+            { title: 'Blue' },
+            { title: 'Green' },
+          ],
+          title: 'Colors',
+        },
+        {
+          items: [{ title: 'Men' }, { title: 'Women' }],
+          title: 'Gender',
+        },
+      ],
+      size: '',
+      newone: '',
+    }
   },
-  methods: {},
+  methods: {
+    check(e) {
+      console.log(e, this.newone)
+    },
+  },
 }
 </script>
+
 <style scoped>
 .image {
   margin: auto;
